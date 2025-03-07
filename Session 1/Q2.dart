18f61d9ca04587dcb647df7dfda1e5bc0369e6a5
AbanopNashat <abanopnashat01@gmail.com>
2025-03-07 23:31:48 +0200

    Session1 homework

---
+++ Session 1/Q2.dart
@@ -0,0 +1,9 @@
+void main(){
+//   Question 2
+//  Explain the purpose of the main function in a Dart application
+// --------------------------------------------------------------------
+// Answer : the main function in most programming languages is where the program
+// starts executing. if we have any snippets of code out of main() function
+// it won't run if we didn't call it in main() function
+// and according to Dart official tutorial it serves as the entrypoint to the app.
+}
